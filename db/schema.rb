4c56dd55303057c9546ffc5d9f81ebc51323edfe
Mahmoud-64 <mahmoudyoussef070@gmail.com>
2020-04-27 01:06:31 +0200

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_190403) do
+ActiveRecord::Schema.define(version: 2020_04_26_225152) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
